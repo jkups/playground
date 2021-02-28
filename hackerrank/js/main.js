cbab71b05448247b68ed61600c7e40a648aecec6
jkups <o.johnkupoluyi@gmail.com>
2021-02-28 16:04:03 +1100

    bomberman js

--- hackerrank/js/main.js
+++ hackerrank/js/main.js
@@ -1,27 +1,26 @@
-// function absolutePermutation(n, k) {
-//     let i = 1, collection = {}, result = [];
-//
-//     for(let l = 1; l <= n; l++){
-//         collection[l] = l
-//     }
-//
-//     while(i <= n){
-//         for(const props in collection){
-//           const item = collection[props]
-//             if(Math.abs(item - i) === k){
-//                 result.push(item);
-//                 console.log(item);
-//                 delete collection[props]
-//                 break;
-//             }
-//         }
-//         if(result.length !== i) return [-1];
-//         i++;
-//     }
-//     return result
-// }
-
 function absolutePermutation(n, k) {
+    let i = 1, collection = {}, result = [];
+
+    for(let l = 1; l <= n; l++){
+        collection[l] = l
+    }
+
+    while(i <= n){
+        for(const props in collection){
+          const item = collection[props]
+            if(Math.abs(item - i) === k){
+                result.push(item);
+                console.log(item);
+                delete collection[props]
+            }
+        }
+        if(result.length !== i) return [-1];
+        i++;
+    }
+    return result
+}
+
+function efficientAbsolutePermutation(n, k) {
   const result = [], collection = {};
   let i = 1
 
@@ -35,3 +34,53 @@ function absolutePermutation(n, k) {
   }
   return result
 }
+
+function bomberMan(n, grid) {
+    let timer = 1, count = 0, bombsToDetonate = [];
+
+    grid.forEach((row,idx) =>  grid[idx] = [...row])
+    // setTimeout(() => {count++}, 2000)
+
+    while(count <= n){
+        switch (timer){
+            case 1:
+            grid.forEach((row,idx) => {
+                bombsToDetonate.push(
+                  row.map((item, idx) => {
+                    if(item === '0') return idx
+                  }).filter(item => item !== undefined)
+                )
+                grid[idx] = row.map(item => '0')
+            })
+            break;
+
+            case 2:
+            bombsToDetonate.forEach((row, idx) => {
+              if(row.length === 0) return
+              for(let i = 0; i < row.length; i++){
+                grid[idx][row[i]] = '.'
+                if(row[i] - 1 >= 0) grid[idx][row[i] - 1] = '.'
+                if(row[i] + 1 < grid[0].length) grid[idx][row[i] + 1] = '.'
+                if(idx - 1 >= 0) grid[idx - 1][row[i]] = '.'
+                if(idx + 1 < grid.length) grid[idx + 1][row[i]] = '.'
+              }
+            })
+            bombsToDetonate = []
+        }
+
+        count++
+        timer = timer < 2 ? timer + 1 : 1;
+        setTimeout(() => {}, 1000)
+    }
+    grid.forEach((row,idx) =>  grid[idx] = row.join(''))
+    return grid
+}
+
+console.log(bomberMan(7, [
+  '.......',
+  '...0.0.',
+  '....0..',
+  '..0....',
+  '00...00',
+  '00.0...'
+]))
